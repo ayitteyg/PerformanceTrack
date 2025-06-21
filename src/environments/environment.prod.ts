53ba5139186d891bd845b788bc51270ef286a23b
ayitteyg <ayitteyg@yahoo.com>
2025-06-21 21:23:33 +0000

    set apiurl for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://baosa07.onrender.com/api'
-  apiUrl: ''
+  apiUrl: 'https://performancetrackapi.onrender.com'
+  //apiUrl: ''
   
 };
